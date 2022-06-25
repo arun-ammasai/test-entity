b74395a9d2d95c21fd5336e0c76c91db1e12bf50
Arun Ammasai <arun.ammasai@outlook.com>
2022-06-25 08:17:27 +0530

    test project for github

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,5 +9,8 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	void test(){
+		System.out.println("Check in 1");
+	}
 
 }
