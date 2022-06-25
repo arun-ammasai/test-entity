9ce4f6ee6f5d2b6c8545210d7f3df6e25e119b32
Arun Ammasai <arun.ammasai@outlook.com>
2022-06-25 08:21:55 +0530

    Step 5

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -14,6 +14,7 @@ public class DemoApplication {
 		System.out.println("Check in 2");
 		System.out.println("Check in 3");
 		System.out.println("Check in 4");
+		System.out.println("Check in 5");
 	}
 
 }
